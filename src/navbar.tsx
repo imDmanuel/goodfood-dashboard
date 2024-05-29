eac30dcc139634124866b6ca2cf011b06c2af386
Albinson Normanson <albinsonnormanson@gmail.com>
2024-05-29 11:26:00 +0100

    worked on navbar mobile - yet to implement UI for profile mobile view in the navbar

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -16,13 +16,13 @@ export default function Navbar({
       <button
         onClick={openSidebar}
         type="button"
-        className="border-0 border-[#C8CBD9] p-0.5 rounded-md aspect-square relative"
+        className="lg:hidden mr-4 border-0 border-[#C8CBD9] p-0.5 rounded-md aspect-square relative"
       >
         <img src={menuIcon} className="w-12" />
       </button>
       {/* END MOBILE MENU ICON */}
       {/* SEARCH */}
-      <div className="bg-[#F6F6FB] flex items-center rounded-md pr-3 focus-within:outline-[#707FDD] focus-within:outline py-2">
+      <div className="max-sm:w-full bg-[#F6F6FB] flex items-center rounded-md pr-3 focus-within:outline-[#707FDD] focus-within:outline py-2">
         <input
           type="text"
           className="bg-transparent focus:outline-none w-[120px] sm:w-[350px] lg:w-[600px] px-4 placeholder:text-[#1F384C] placeholder:text-opacity-30 placeholder:text-xs placeholder:font-normal text-[#1F384C]"
@@ -32,21 +32,23 @@ export default function Navbar({
       </div>
       {/* END SEARCH */}
 
-      {/* ACCOUNT INFO */}
-      <div className="ml-auto flex items-center mr-8">
-        <div className="bg-[#FFE6CC] size-8 flex items-center justify-center rounded-full">
-          <img src={burgerEmoticon} alt="" className="w-4" />
+      <div className="max-sm:hidden ml-auto">
+        {/* ACCOUNT INFO */}
+        <div className="flex items-center">
+          <div className="bg-[#FFE6CC] size-8 flex items-center justify-center rounded-full">
+            <img src={burgerEmoticon} alt="" className="w-4" />
+          </div>
+
+          <div className="text-xs text-[#1F384C] ml-3">Delicious Burger</div>
+
+          <img src={chevronDownIcon} alt="" className="size-5" />
         </div>
+        {/* END ACCOUNT INFO */}
 
-        <div className="text-xs text-[#1F384C] ml-3">Delicious Burger</div>
-
-        <img src={chevronDownIcon} alt="" className="size-5" />
+        {/* NOTIFICATION ICON */}
+        <img src={notificationIcon} className="w-4" />
+        {/* END NOTIFICATION ICON */}
       </div>
-      {/* END ACCOUNT INFO */}
-
-      {/* NOTIFICATION ICON */}
-      <img src={notificationIcon} className="w-4" />
-      {/* END NOTIFICATION ICON */}
     </nav>
   );
 }
